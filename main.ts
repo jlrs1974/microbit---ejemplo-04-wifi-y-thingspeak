85ce973662bc32d53ef364e9a3a740ff5526e22b
jlrs1974 <114936060+jlrs1974@users.noreply.github.com>
2022-12-05 13:25:49 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("MSI-JLRS", "050191gemma")
-let contador = 0
 basic.clearScreen()
 OLED.init(128, 64)
 OLED.clear()
@@ -8,18 +7,11 @@ basic.forever(function () {
     basic.pause(60000)
     ESP8266_IoT.connectThingSpeak()
     if (ESP8266_IoT.wifiState(true) && ESP8266_IoT.thingSpeakState(true)) {
-        contador = contador + 1
         ESP8266_IoT.setData(
         "RY1W34VOS4Q56GZB",
-        input.temperature(),
-        contador,
-        input.lightLevel()
+        input.temperature()
         )
         ESP8266_IoT.uploadData()
-        basic.showNumber(contador)
-        OLED.writeString("Contador: ")
-        OLED.writeNum(contador)
-        OLED.writeString(" - ")
         OLED.writeString("Hora: ")
         OLED.writeString(timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr))
         OLED.writeString(" - ")
